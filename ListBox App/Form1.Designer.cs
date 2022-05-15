5f11843883d3b5d1be7de12c7d3956b8d5e599a9
Abdirizak Mohamed <halac122@gmail.com>
2022-05-15 19:58:48 +0300

    Added New Features

--- ListBox App/Form1.Designer.cs
+++ ListBox App/Form1.Designer.cs
@@ -30,25 +30,22 @@
         {
             this.lstMyListBox = new System.Windows.Forms.ListBox();
             this.btnSelect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lstMyListBox
             // 
             this.lstMyListBox.FormattingEnabled = true;
             this.lstMyListBox.ItemHeight = 15;
-            this.lstMyListBox.Items.AddRange(new object[] {
-            "Hargeisa",
-            "Burao",
-            "Borama",
-            "Berbera"});
-            this.lstMyListBox.Location = new System.Drawing.Point(9, 20);
+            this.lstMyListBox.Location = new System.Drawing.Point(380, 12);
             this.lstMyListBox.Name = "lstMyListBox";
             this.lstMyListBox.Size = new System.Drawing.Size(322, 364);
             this.lstMyListBox.TabIndex = 0;
             // 
             // btnSelect
             // 
-            this.btnSelect.Location = new System.Drawing.Point(128, 406);
+            this.btnSelect.Location = new System.Drawing.Point(174, 68);
             this.btnSelect.Name = "btnSelect";
             this.btnSelect.Size = new System.Drawing.Size(75, 23);
             this.btnSelect.TabIndex = 1;
@@ -56,16 +53,35 @@
             this.btnSelect.UseVisualStyleBackColor = true;
             this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Color:";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(90, 19);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(284, 23);
+            this.txtColor.TabIndex = 3;
+            // 
             // Demo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(347, 441);
+            this.ClientSize = new System.Drawing.Size(714, 441);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSelect);
             this.Controls.Add(this.lstMyListBox);
             this.Name = "Demo";
             this.Text = "Demo";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,5 +89,7 @@
 
         private ListBox lstMyListBox;
         private Button btnSelect;
+        private Label label1;
+        private TextBox txtColor;
     }
 }
--- ListBox App/Form1.cs
+++ ListBox App/Form1.cs
@@ -9,12 +9,20 @@ namespace ListBox_App
 
         private void btnSelect_Click(object sender, EventArgs e)
         {
-            // Declare varaible hold city name 
+            /* Declare varaible hold city name 
             string name; 
 
             name = lstMyListBox.Items[lstMyListBox.SelectedIndex].ToString();
 
             MessageBox.Show(name);
+
+         
+
+            lstMyListBox.Sorted = false;
+
+              */
+
+            lstMyListBox.Items.Add(txtColor.Text);
 
 
         }
