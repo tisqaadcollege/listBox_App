250d44b9fe60bddbe6a5766e05119afa3efc8800
Abdirizak Mohamed <halac122@gmail.com>
2022-05-15 19:53:24 +0300

    first commit

---
+++ ListBox App/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ListBox_App
+{
+    partial class Demo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstMyListBox = new System.Windows.Forms.ListBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstMyListBox
+            // 
+            this.lstMyListBox.FormattingEnabled = true;
+            this.lstMyListBox.ItemHeight = 15;
+            this.lstMyListBox.Items.AddRange(new object[] {
+            "Hargeisa",
+            "Burao",
+            "Borama",
+            "Berbera"});
+            this.lstMyListBox.Location = new System.Drawing.Point(9, 20);
+            this.lstMyListBox.Name = "lstMyListBox";
+            this.lstMyListBox.Size = new System.Drawing.Size(322, 364);
+            this.lstMyListBox.TabIndex = 0;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(128, 406);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // Demo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 441);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lstMyListBox);
+            this.Name = "Demo";
+            this.Text = "Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListBox lstMyListBox;
+        private Button btnSelect;
+    }
+}
---
+++ ListBox App/Form1.cs
@@ -0,0 +1,22 @@
+namespace ListBox_App
+{
+    public partial class Demo : Form
+    {
+        public Demo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            // Declare varaible hold city name 
+            string name; 
+
+            name = lstMyListBox.Items[lstMyListBox.SelectedIndex].ToString();
+
+            MessageBox.Show(name);
+
+
+        }
+    }
+}
